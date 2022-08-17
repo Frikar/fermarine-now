05ec3464f281ba7c9f485f6089eb18a5eaf2383d
klimker <carloseur24@gmail.com>
2022-08-17 17:00:12 -0400

    first commit

---
+++ assets/config.js
@@ -0,0 +1,39 @@
+// cpanel - site_templates/countdown_tech/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        email: "Ventas@fermarines.com",
+        logo: "FERMARINE",
+        social: [
+            
+            
+            
+            {
+                icon: 'instagram',
+                link: "https:\/\/instagram.com\/fermarinesnavales?igshid=YmMyMTA2M2Y="
+            },
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+    },
+    slides: [
+        {
+            type: 'countdown',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            endTime: '2022-08-23T19:46:00.000Z',
+            content: "Próximamente disponibles a través de nuestro sitio web",
+        }
+    ]
+};
